dbafcb9d91c693965f22d4dd9bf2793fabb4b5af
Isabel Harms <64422293+IsabelHarms@users.noreply.github.com>
2023-12-13 20:00:59 +0100

    Update main.cu

--- main.cu
+++ main.cu
@@ -1,97 +1,172 @@
 #include <cstdio>
 #include <cstdint>
 #include <cassert>
+#include <chrono>
 
 #include <cuda_runtime.h>
 
 
-const int SectionSize = 100000;
-const int SectionCount = 10; // 250 or 256 ?
+const int SectionSize = 10000;
+const int SectionCount = 100000; // 250 or 256 ?
+const int  length = SectionCount * SectionSize;
 // 10 000 000: count = 664579
 
-__global__ static void primeCount(int sectionNr, float *primes) // count primes in section
+__global__ static void primeCountParallelSections(bool *primes) // count primes in section
 {
-    unsigned int start_time = 0, stop_time = 0;
-    start_time = clock();
-    int count = 0;
-    int start = sectionNr * SectionSize;
-    int stop = start + SectionSize;
-    if (start < 2) start = 2;
-
-    for (int test = start; test < stop; test++) // ist "test" a prime number?
+    unsigned int start = (blockIdx.x * blockDim.x + threadIdx.x) * SectionSize;
+    unsigned stop = start + SectionSize;
+    for (unsigned test = start; test < stop; test++) // is "test" a prime number?
     {
-        bool prime = true;
-        primes[test] = 1;
+        primes[test] = true;
         for (int factor = 2; factor * factor <= test; factor++) // stop at square root
         {
-            if (test % factor != 0) continue;
-            prime = false; // devider found!
-            primes[test] = 0;
-            break;
+            if (test % factor == 0) {
+                primes[test] = false;
+                break;
+            }
         }
-        if (prime) count++;
     }
-    stop_time = clock();
-    printf("section %d: %d, time: %d ms\n", sectionNr, count, stop_time-start_time);
 }
 
-__global__ static void primeSieve(int sectionNr)
+__global__ static void primeCountParallel(bool *primes) // count primes in section
 {
-    unsigned int start_time = 0, stop_time = 0;
-    start_time = clock();
-    int start = sectionNr * SectionSize;
-    int stop = start + SectionSize;
+    unsigned number = (blockIdx.x * blockDim.x + threadIdx.x);
+    for (int factor = 2; factor * factor <= number; factor++) {
+        if (number % factor == 0) {
+            primes[number] = false;
+            break;
+        }
+    }
+}
 
-    bool prime[SectionSize];
-    // stack data in c and c++ must be initialized!
-    for (int i = 0; i < SectionSize; i++) prime[i] = true;
-    if (sectionNr == 0) prime[0] = prime[1] = false;
+static void primeCount(bool *primes)
+{
+    for (int number = 2; number < length; number++) {
+        for (int factor = 2; factor * factor <= number; factor++) {
+            if (number % factor == 0) {
+                primes[number] = false;
+                break;
+            }
+        }
+    }
+}
 
-    for (int factor = 2; 2*factor<stop; factor++)
+static void primeCountOptimized(bool *primes)
+{
+    for (int number = 2; number < length; number++) {
+            for (int factor = 2; factor * factor <= number; factor++) {
+                if (primes[factor] && number % factor == 0) {
+                    primes[number] = false;
+                    break;
+                }
+            }
+    }
+}
+
+__global__ static void primeSieveParallelSections(bool *primes)
+{
+    unsigned start = (blockIdx.x * blockDim.x + threadIdx.x) * SectionSize;
+    unsigned stop = start + SectionSize;
+
+    for (int factor = 2; 2*factor<=stop; factor++)
     {
-        int multiple = start / factor;
+        unsigned multiple = start / factor;
         if (multiple < 2) multiple = 2;
-        int i = multiple * factor;
+        unsigned i = multiple * factor;
         if (i < start) i += factor; // eg. start at 10: 10/3 = 3; 3*3 = 9; --> start at 12
 
         while (i < stop)
         {
-            prime[i - start] = false;
+            primes[i] = false;
             i += factor;
         }
     }
-    int count = 0;
-    for (int i = 0; i < SectionSize; i++) {
-        if (prime[i]) count++;
+}
+
+__global__ static void primeSieveParallel(bool *primes)
+{
+    unsigned factor = threadIdx.x + blockIdx.x * blockDim.x;
+    if (factor >= 2) {
+        for (unsigned i = factor+factor; i<length; i+= factor)
+        {
+            primes[i] = false;
+        }
     }
-    stop_time = clock();
-    printf("section %d: %d, time: %d ms\n", sectionNr, count, stop_time-start_time);
+}
+
+static void primeSieve(bool *primes) // count primes in section
+{
+    for (int factor = 2; factor < length/2; factor++) {
+        for (int i = factor+factor; i<length; i+= factor)
+        {
+            primes[i] = false;
+        }
+    }
+}
+
+static void primeSieveOptimized(bool *primes) // count primes in section
+{
+    for (int factor = 2; factor < length/2; factor++) {
+        if (primes[factor])
+        {
+            int multiple = 2 * factor;
+            while (multiple < length) { primes[multiple] = false; multiple += factor; }
+        }
+    }
 }
 
 int main()
 {
-    int size = SectionSize * SectionCount * sizeof(float);
-
-    float *h_primes;
-    float *d_primes;
-
-    h_primes = (float*)malloc(size);
-    cudaMalloc( (void **) &d_primes, size);
-    cudaMemcpy(d_primes, h_primes, size, cudaMemcpyHostToDevice);
-
+    auto begin = std::chrono::high_resolution_clock::now();
     int count = 0;
+    bool *h_primes;
+    h_primes = (bool*)malloc(length);
+    for (int i = 0; i < length; i++) h_primes[i] = true;
 
-    for (int section = 0; section < SectionCount; section++)
-    {
-        //count += PrimeSieve(section);
-        cudaStream_t stream;
-        cudaStreamCreate(&stream);
-        primeSieve<<<1, 1, 0, stream>>>(section);
-        //primeCount<<<1, 1, 0, stream>>>(section, d_primes);
-        cudaStreamDestroy(stream);
-    }
+    int deviceId;
+    int numberOfSMs;
+    cudaGetDevice(&deviceId);
+    cudaDeviceGetAttribute(&numberOfSMs, cudaDevAttrMultiProcessorCount, deviceId);
+    size_t threadsPerBlock;
+    size_t numberOfBlocks;
+    threadsPerBlock = 100;
+    numberOfBlocks = length/100;//32 * numberOfSMs; //length/threadsPerBlock;//
+
+    bool *d_primes;
+    cudaMalloc( (void **) &d_primes, length);
+    cudaMemcpy(d_primes, h_primes, length, cudaMemcpyHostToDevice);
+    cudaStream_t stream;
+    cudaStreamCreate(&stream);
+
+    //primeSieveOptimized(h_primes);
+    primeCountParallel<<<numberOfBlocks,threadsPerBlock,0,stream>>>(d_primes);
+
     cudaDeviceSynchronize();
-    cudaMemcpy( h_primes, d_primes, size, cudaMemcpyDeviceToHost);
+    cudaMemcpy( h_primes, d_primes, length, cudaMemcpyDeviceToHost);
+    cudaStreamDestroy(stream);
     cudaFree(d_primes);
+
+    h_primes[0] = h_primes[1] = false;
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    for (int i = 0; i < SectionSize * SectionCount; i++) {
+       if (h_primes[i]) count++;
+    }
+    printf("%d, %.3f",count, elapsed.count() * 1e-9);
+
     free(h_primes);
 }
+
+/*
+ *
+    //primeCount(h_primes);
+ *   //primeSieve(h_primes);
+    //
+    //primeSieveOptimized(h_primes);
+    //primeSieveParallel<<<length/2, 1, 0, stream>>>(d_primes);
+
+
+
+
+ * */
